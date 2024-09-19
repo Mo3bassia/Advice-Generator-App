17120c3786e7e5083c56a5a700e92b5c076d51d1
Mohamed Abassia <mo3bassia@gmail.com>
2024-09-19 18:00:00 +0300

    Finish Js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,22 @@
-fetch("https://api.adviceslip.com/advice")
-  .then((result) => result.json())
-  .then((result) => {
-    console.log(result);
-  });
+let adviceId = document.querySelector(".advice-id span");
+let adviceTitle = document.querySelector(".advice-title");
+let nextEl = document.querySelector(".next");
+
+let getAdvice = async () => {
+  let result = await fetch("https://api.adviceslip.com/advice");
+  let json = await result.json();
+  adviceTitle.textContent = json.slip.advice;
+  adviceId.textContent = json.slip.id;
+};
+// fetch("https://api.adviceslip.com/advice")
+//   .then((result) => result.json())
+//   .then((result) => {
+//     console.log(result);
+//   });
+
+getAdvice();
+
+nextEl.onclick = function () {
+  console.log("HELLO");
+  getAdvice();
+};
